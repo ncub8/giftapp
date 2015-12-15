2d119f88265996dc8e0d870d322be51e20d7ae55
John Moore <john@ncub8.com>
2015-12-14 22:29:11 -0500

    add view

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,14 @@ router.get('/', function(req, res, next) {
 router.get('/show', function(req, res, next) {
     var db = req.db;
     var collection = db.get('users');
-    collection.find({},{},function(err,docs){
+    collection.find({}, {}, function(err,docs){
         if(!err){
-            res.json(docs);
+            //res.json(docs);
+            res.render('users/show', { users: docs });
         }else{
             res.send('error');
         }
     });
 });
-
 
 module.exports = router;
